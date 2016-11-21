7f2347b30c27aa42233c1643c09f5df055eaf8a4
yummykang <yummykang@163.com>
2016-11-21 16:57:23 +0800

    添加modal

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
-var scotchApp = angular.module('scotchApp', ['ngRoute']);
+var scotchApp = angular.module('scotchApp', ['ngRoute', 'ui.bootstrap']);
 
 scotchApp.config(["$routeProvider", function ($routeProvider) {
-    $routeProvider.
-    when('/', {
-        templateUrl : 'index.html'
-    }).
-    when('/orders', {
-        templateUrl : 'business/orders/orders.html'
+    $routeProvider.when('/', {
+        templateUrl: 'index.html'
+    }).when('/orders', {
+        templateUrl: 'business/orders/orders.html'
     });
 }]);
 
-scotchApp.controller('scotchController', function ($scope, $http) {
+scotchApp.controller('scotchController', function ($scope, $http, $modal) {
     $http.get("json/menu.json").success(function (data) {
         $scope.menus = data;
     });
+    $scope.open = function () {
+        var modalInstance = $modal.open({
+            templateUrl: 'business/orders/order_details.html'
+        })
+    };
 });
